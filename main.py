210e58a991b1303429568cfe7f80826ed2d050ae
pandalime405-boop <pandalime405@gmail.com>
2025-12-11 17:35:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime
 
 TOKEN = "8354172231:AAEq2OuXq45Klg3ekYa1CNS8spqYRhbwGac"
 CHAT_ID = -1001234567890  # id класної бесіди
 
 students = [
     "Андрусик Ангеліна",
-    ".Болюк Денис",
+    "Болюк Денис",
     "Ватаг Руслан",
     "Веселик Оксана",
     "Візнюк Назар",
@@ -31,43 +30,41 @@ students = [
     "Феделеш Юрій",
     "Яцик Данило",
     "Яцура Матвій"
-    
-    
 ]
 
 index = 0  # початок циклу
 
 
-async def send_duty():
+async def send_duty(bot: Bot):
     global index
-    bot = Bot(token=TOKEN)
 
-    # беремо 2 учнів зі списку по черзі
-    duty = [students[index % len(students)], students[(index + 1) % len(students)]]
+    duty = [
+        students[index % len(students)],
+        students[(index + 1) % len(students)]
+    ]
     index += 2
 
     text = f"Чергові: {duty[0]}, {duty[1]}"
-
     await bot.send_message(CHAT_ID, text)
-    await bot.session.close()
 
 
 async def main():
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
     scheduler = AsyncIOScheduler(timezone="Europe/Kyiv")
-
     scheduler.add_job(
         send_duty,
         trigger="cron",
         day_of_week="mon-fri",
-        hour=17,
-        minute=23
+        hour=8,
+        minute=20,
+        args=[bot]                 # <<<<<< ОБОВʼЯЗКОВО передаємо bot
     )
-
     scheduler.start()
-    await dp.start_polling(Bot(TOKEN))
+
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyn
